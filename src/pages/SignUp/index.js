8107aac906b038fac00422a5c2c238e88d184d72
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2020-06-17 11:00:12 -0300

    criada pagina de signup

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,14 +1,51 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-
+import { Image } from 'react-native';
+import logo from '~/assets/logo.png';
 import Background from '~/components/Background';
+import {
+  Container,
+  Form,
+  FormInput,
+  SignLink,
+  SignLinkText,
+  SubmitButton,
+} from './styles';
 
-export default function SignUp() {
+export default function SignUp({ navigation }) {
   return (
     <Background>
-      <View>
-        <Text>SignUp</Text>
-      </View>
+      <Container>
+        <Image source={logo} />
+
+        <Form>
+          <FormInput
+            icon="person-outline"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Nome completo"
+          />
+
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha secreta"
+          />
+        </Form>
+
+        <SubmitButton onPress={() => {}}>Criar Conta</SubmitButton>
+
+        <SignLink onPress={() => navigation.navigate('SignIn')}>
+          <SignLinkText>Voltar para o login</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 }
